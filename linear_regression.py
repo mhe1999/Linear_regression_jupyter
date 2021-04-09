e8093f5f851bcfcdaacd895dbf4bdb443ee18ebc
mhe1999 <ehteshami1378@gmail.com>
2021-04-09 21:04:55 +0430

    multi-variable linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -64,7 +64,7 @@ def partition_data(dataset,train_percent = 0.6, CV_percent = 0.2, test_percent =
 def compute_cost(X, y, theta):
     m = X.shape[0]
     yhat = np.dot(X, theta)
-    lost = np.power(yhat- y, 2)
+    lost = np.power(yhat - y, 2)
     cost = 1/(2*m) * np.sum(lost)
     # print(cost)
     return cost
@@ -126,22 +126,59 @@ def one_variable_linear_regression(dataset,inital_theta, itteration = 1500, alph
         cost_save= np.append(cost_save, cost)
     return cost_save, theta
 
+def linear_regression(dataset,inital_theta, itteration = 1500, alpha = 0.01, FN=0):
+    m = dataset.shape[0]
+    theta = inital_theta
+    X = dataset[:,0:-1]
+    # print(dataset)
+    # print(X)
+    # print(dataset.shape)
+    # print(X.shape)
+    X, X_mu, X_sigma = feature_normalization(X)
+    X = np.append(np.zeros((m,1)) + 1, X, axis = 1) # add x0
+    # print(X)
+    y = np.reshape(dataset[:,-1], (-1,1))
+    # print(y)
+    cost_save = np.array([])
+    for i in range(itteration):
+        gradients = cal_gradients(X, y, theta)
+        theta = update_parameters(gradients, theta, alpha)
+        cost = compute_cost(X, y, theta)
+        # print(cost)
+        cost_save= np.append(cost_save, cost)
+    return cost_save, theta, X_mu, X_sigma
+
 if __name__ == '__main__':
-    # dataset = np.array([[1,"A", "Aa", 10], [2,"B", "Bb", 20], [3,"C", "Cc", 30]], dtype=object)
-    # print(int_encoder(dataset))
-    # print(oneHat_encoder(dataset))
 
     data_all = load_data()
+    dataset = data_all
+    # dataset = np.append(data_all[:, 18:24], np.reshape(data_all[:, 25], (-1,1)), axis=1)
+    dataset = oneHat_encoder(dataset)
+    # print(dataset)
+    train_data, CV_data, test_data = partition_data(dataset,train_percent = 0.999, CV_percent = 0, test_percent = 0.1)
+    n = train_data.shape[1]
+    theta = np.zeros((n,1))
+    c , t, mu, sigma = linear_regression(train_data, theta, 5000, 0.175)
+
+    plt.plot(c)
+    plt.show()
+
+    # print(train_data[:, 0:-1])
+    yhat = predict(test_data[:, 0:-1], t, mu, sigma)
+    # print(c)
+    print("y:",test_data[:, -1])
+    print("yhat:", yhat)
+
     # train_data, CV_data, test_data = partition_data(dataset)
     # single
     # train_data = dataset
-    dataset = np.append(np.reshape(data_all[:, 21], (-1,1)), np.reshape(data_all[:, 25], (-1,1)), axis=1)
+    # dataset = np.append(np.reshape(data_all[:, 21], (-1,1)), np.reshape(data_all[:, 25], (-1,1)), axis=1)
     # print(sv_dataset)
     # print (CV_data, CV_data.shape)
-    theta = np.zeros((2,1))
+    # theta = np.zeros((2,1))
     # theta[0][0] = 5613
     # theta[1][0] = 187
-    c, t= one_variable_linear_regression(dataset, theta, 100, 0.00001)
+    # c, t= one_variable_linear_regression(dataset, theta, 100, 0.00001)
     # print(t)
     # plt.plot(train_data[:, 21], train_data[:, 25], 'o')
     # plt.plot(c)
@@ -154,8 +191,7 @@ if __name__ == '__main__':
     # x1, y1 = [5, 12], [1, 4]
     # x2, y2 = [1, 10], [3, 2]
     # plt.plot(x1,y1,x2,y2, marker='o')
-    plt.plot(data_all[:, 21],data_all[:, 25], 'yo', data_all[:, 21], t[0] + data_all[:, 21] * t[1], '--k')
-
-    plt.show()
-    plt.plot(c)
-    plt.show()
+    # plt.plot(data_all[:, 21],data_all[:, 25], 'yo', data_all[:, 21], t[0] + data_all[:, 21] * t[1], '--k')
+    # plt.show()
+    # plt.plot(c)
+    # plt.show()
